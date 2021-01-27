397df5d92e71751e3bf8c29bb62e6ed7d76c9a8b
Sukumar_Banda <sukumarbanda@gmail.com>
2021-01-27 20:06:34 +0530

    collections-strings

--- src/com/string/examples/CountCharacters.java
+++ src/com/string/examples/CountCharacters.java
@@ -24,9 +24,9 @@ public class CountCharacters {
 				obj.put(c, 1);
 			}
 		}
-		
+		System.out.println("Character count");
 		System.out.println(obj);
-		
+		System.out.println("==================");
 		
 		
 	}
