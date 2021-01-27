fa92d95c1b8232c24603d9e9d5f2f36ed97ec1b9
Sukumar_Banda <sukumarbanda@gmail.com>
2021-01-27 19:35:49 +0530

    collections

---
+++ src/com/string/examples/CountCharacters.java
@@ -0,0 +1,34 @@
+package com.string.examples;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountCharacters {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter the String: ");
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		
+		char[] ch = str.toCharArray();
+		
+		HashMap<Character,Integer> obj = new HashMap<Character,Integer>();
+		
+		
+		for (char c : ch) {
+			if(obj.containsKey(ch))
+			{
+				obj.put(c, obj.get(c)+1);
+			}
+			else {
+				obj.put(c, 1);
+			}
+		}
+		
+		System.out.println(obj);
+		
+		
+		
+	}
+	
+}
